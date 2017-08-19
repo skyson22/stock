2229b3e169e845ed520e7ed9452566fb5e3fdbb0
jimmy <skyson22@hotmail.com>
2017-08-19 20:07:47 +0800

    1: add __main__.py

---
+++ wmm/__init__.py
@@ -0,0 +1,5 @@
+'''
+Created on 2017¦~8¤ë19¤é
+
+@author: haha
+'''
---
+++ wmm/__main__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from wmm.data.tw_stock import TwStock
+
+def initLogging():
+    FORMAT = '[%(asctime)s][%(levelname)s] %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT, filename='log/system.log')
+     
+def main():
+    initLogging()
+
+    test = TwStock()
+    test.updateDB()
+
+if __name__ == '__main__':
+    main()
